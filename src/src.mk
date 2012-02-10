7edd42bfa96428d760dcec7217077ae1f84ea149
Aleksandar Babic <aleksandar.babic@met.no>
2012-02-10 13:30:33 +0100

    defining some subdirs

---
+++ src/src.mk
@@ -0,0 +1,21 @@
+
+#-----------------------------------------------------------------------------
+# WDB FeltLoad Component
+#-----------------------------------------------------------------------------
+
+feltPointLoad_SOURCES =  src/main.cpp \
+					     src/feltConstants.h \
+					     src/feltTypeConversion.h \
+					     src/feltTypeConversion.cpp \
+					     src/FeltFile.cpp \
+					     src/FeltFile.h \
+					     src/FeltField.cpp \
+					     src/FeltField.h \
+					     src/FeltLoader.cpp \
+					     src/FeltLoader.h \
+					     src/FeltGridDefinition.cpp \
+					     src/FeltGridDefinition.h \
+					     src/FeltLoadConfiguration.cpp \
+					     src/FeltLoadConfiguration.h
+	
+EXTRA_DIST +=		src/src.mk
